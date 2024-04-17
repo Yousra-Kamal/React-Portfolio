c1d3df6a95685ac1e47083758a8c13e396b7cb9e
Yousra <nice.sora@hotmail.com>
2024-04-17 16:29:01 +1000

    Created Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Footer from "./components/Footer"; // Import the 'Footer' component
 
 function App() {
   return (
     <div className="flex justify-center">
-      <h1 className="text-3xl font-bold underline text-orange-400">
-        Yousra's portfolio
-      </h1>
+      <Footer/>
     </div>
   );
 }
